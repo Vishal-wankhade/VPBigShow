a9da5da0e55c72007fdb6797252f1049fdfac3c7
vishal wankhade <vwankhade120@gmail.com>
2024-03-05 19:39:54 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from "react";
 
-function App() {
+import "./App.css"; // Make sure to import your CSS file
+// import cover from './videos/cover.png'
+import ReactPlayer from 'react-player'
+import playlist from "./Playlist";
+import { BsFillCollectionPlayFill } from "react-icons/bs";
+import { MdMovieFilter } from "react-icons/md";
+
+const App = () => {
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function PlayThisVideo(index) {
+    setCurrentVideoIndex(index);
+  }
+
+  const filteredPlaylist = playlist.filter((video) =>
+    video.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App font-mono">
+      <div className="header flex items-center justify-center">
+      <BsFillCollectionPlayFill className="text-4xl mr-4" />
+         <h1 className="text-4xl font-bold ">The BigShow</h1>
+         <BsFillCollectionPlayFill className="text-4xl ml-4" />
+      </div>
+      <div className="container">
+         <div className="mainframe flex items-center  justify-around flex-col">
+           <ReactPlayer url={playlist[currentVideoIndex].videoUrl} width='95%'
+          height='100%' autoplay light={<img src={playlist[currentVideoIndex].thumbnailUrl} alt='Thumbnail' className="cover" />}  controls className="player shadow-lg  rounded-xl mt-30" />
+            <div className="videoInfo p-4">
+               <div className="flex justify-around head">
+               <MdMovieFilter className="mr-5 icon" />
+                <h1 className="text-4xl text-center flex font-bold title">{playlist[currentVideoIndex].title}</h1>
+               </div>
+               <h3 className="text-center pt-5 text-justify" width="60%">{playlist[currentVideoIndex].Desc}</h3>
+            </div>
+         </div>
+         <div className="playlist flex flex-col">
+           <input
+            type="text"
+            placeholder="Search by video title"
+            className="search p-2 border-0 outline-0 rounded-xl"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          {
+            filteredPlaylist.map((video,index) =>
+            {
+              return (
+              <div className={`list flex rounded-xl m-5 p-2 cursor-pointer shadow-lg ${
+                currentVideoIndex === index ? "active" : ""
+              }`} key={index}  onClick={() => PlayThisVideo(index)}>
+                <img src={video.thumbnailUrl}  alt="cover" />
+                <div className="ListvideoInfo">
+                  <h4 className="font-bold pl-5 title">{video.title}</h4>
+                  <p className="pl-5 p-1 font-semibold">{video.Desc ? video.Desc.substring(0, 20) : ''}</p>
+
+                </div>
+              </div> )
+            })
+          }
+
+          </div>
+
+      </div>
+      
+      {/* <VideoPlayer playlist={playlist} /> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Playlist.js
@@ -0,0 +1,68 @@
+import video1 from './videos/cor.mp4';
+import video2 from './videos/hypestrew.mp4'
+import company from './videos/company.png'
+import hypestrew from './videos/hypestrew.png'
+import cover1 from './videos/cover.png' 
+import chaleyaVideo from './videos/chaleya.mp4'
+import chaleyaCover from './videos/chaleyaCover.jpg'
+import bts from './videos/bts.jpg'
+import dynamite from './videos/dynamite.mp4'
+
+
+
+ const Playlist = [
+  {
+    title: 'BTS (방탄소년단) Dynamite Official MV',
+    videoUrl: dynamite,
+    thumbnailUrl:bts,
+    Desc:"“Dynamite” is a Disco Pop single that sings of joy and confidence, bringing a new surge of much-needed ‘energy’ to reinvigorate the global community. In this song, the global superstars find happiness by doing again what they do best: engaging in a perpetual cycle of giving and receiving joy with the world through music and performances.",
+  },
+  
+  {
+    title: 'Chaleya : Jawan new Song',
+    videoUrl: chaleyaVideo,
+    thumbnailUrl:chaleyaCover,
+    Desc:"Witness the love with Chaleya, presenting the second song from the highly-awaited film Jawan directed by Atlee. The film stars Shah Rukh Khan, Vijay Sethupathi, NayantharaLorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  
+  {
+    title: 'How a Company Succeed ?',
+    videoUrl: video1,
+    thumbnailUrl:company,
+    Desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    title: 'Hypestrew Private Limited',
+    videoUrl: video2,
+    thumbnailUrl:hypestrew,
+    Desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+
+  },
+  {
+    title: 'How a Company Succeed ?',
+    videoUrl: video1,
+    thumbnailUrl:company,
+    Desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    title: 'How a Company Succeed ?',
+    videoUrl: video1,
+    thumbnailUrl:company,
+    Desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    title: 'How a Company Succeed ?',
+    videoUrl: video1,
+    thumbnailUrl:company,
+    Desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    title: 'How a Company Succeed ?',
+    videoUrl: video1,
+    thumbnailUrl:company,
+    Desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  // Add more videos as needed
+];
+
+export default Playlist;
